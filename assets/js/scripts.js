8a7d276ca60af5e81f426a984dbaa3cc306bfcb0
Björn Martensen <bjoern.martensen@gmail.com>
2012-08-14 09:09:06 +0200

    added editing contacts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -35,6 +35,7 @@
     	tagName: "article",
     	className: "contact-container",
     	template: $("#contactTemplate").html(),
+    	editTemplate: _.template($("#contactEditTemplate").html()),
 
 		render: function() {
     		var tmpl = _.template(this.template);
@@ -44,7 +45,11 @@
     	},
 
     	events: {
-    		"click button.delete": "deleteContact"
+    		"click button.delete": "deleteContact",
+    		"click button.edit": "editContact",
+			"change select.type": "addType",
+			"click button.save": "saveEdits",
+			"click button.cancel": "cancelEdit"
     	},
 
     	deleteContact: function() {
@@ -57,7 +62,67 @@
     		if (_.indexOf(directory.getTypes(), removedType) === -1) {
     			directory.$el.find("#filter select").children("[value='" + removedType + "']").remove();
     		}
-    	}
+    	},
+
+    	editContact: function() {
+    		this.$el.html(this.editTemplate(this.model.toJSON()));
+
+    		var newOpt = $("<option/>", {
+    			html: "<em>Add new...</em>",
+    			value: "addType"
+    		});
+
+    		this.select = directory.createSelect().addClass("type")
+    			.val(this.$el.find("#type").val()).append(newOpt)
+    			.insertAfter(this.$el.find(".name"));
+
+    		this.$el.find("input[type='hidden'").remove();
+    	},
+
+    	addType: function() {
+    		if (this.select.val() === "addType") {
+			    this.select.remove();
+			 
+			    $("<input />", {
+			        "class": "type"
+			    }).insertAfter(this.$el.find(".name")).focus();
+			}
+    	},
+
+    	saveEdits: function(e) {
+    		e.preventDefault();
+
+    		var formData = {},
+		        prev = this.model.previousAttributes();
+		 
+		    $(e.target).closest("form").find(":input").add(".photo").each(function () {
+		 
+		        var el = $(this);
+		        formData[el.attr("class")] = el.val();
+		    });
+		 
+		    if (formData.photo === "") {
+		        delete formData.photo;
+		    }
+		 
+		    this.model.set(formData);
+		 
+		    this.render();
+		 
+		    if (prev.photo === "/img/placeholder.png") {
+		        delete prev.photo;
+		    }
+		 
+		    _.each(contacts, function (contact) {
+		        if (_.isEqual(contact, prev)) {
+		            contacts.splice(_.indexOf(contacts, contact), 1, formData);
+		        }
+		    });
+    	},
+
+    	cancelEdit: function () {
+		    this.render();
+		},
 	});
 
 	//define master view
